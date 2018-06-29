93b045030edc6644357d2d2305231530cbfc8467
Vanessa Toro <vanetoro84@gmail.com>
2018-06-29 17:13:29 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
 end
